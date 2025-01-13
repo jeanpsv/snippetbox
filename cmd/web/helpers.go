b56eef8add4af13aea9b7d8c18fc1ba2d84eaba7
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2025-01-13 20:42:41 -0300

    add stack trace in error handling

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"runtime/debug"
 
 	"github.com/jeanpsv/snippetbox/config"
 )
@@ -9,8 +10,9 @@ import (
 func serverError(app *config.Application, w http.ResponseWriter, r *http.Request, err error) {
 	method := r.Method
 	uri := r.URL.RequestURI()
+	trace := string(debug.Stack())
 
-	app.Logger.Error(err.Error(), "method", method, "uri", uri)
+	app.Logger.Error(err.Error(), "method", method, "uri", uri, "trace", trace)
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
